0bf71e112758e24d097e770e34c39c876038914b
SirihPinang1407 <sirihpinang1407@gmail.com>
2025-10-16 19:09:39 +0800

    added new modal

---
+++ src/components/NewPostModal.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { jwtDecode } from "jwt-decode";
+
+export default function NewPostModal({ show, handleClose }) {
+    const [postContent, setPostContent] = useState("");
+
+    const handleSave = () => {
+        const token = localStorage.getItem("authToken");
+
+        const decode = jwtDecode(token);
+        const userId = decode.id;
+
+        const data = {
+            title: "Post Title",
+            content: postContent,
+            user_id: userId
+        };
+
+        axios
+            .post("https://e0dc5cb0-de85-4b0b-a831-1d38b0384bcf-00-1vz07zgn5c1sd.pike.replit.dev/posts", data)
+            .then((response) => {
+                console.log("success:", response.data);
+                handleClose();
+            })
+            .catch((error) => {
+                console.error("error", error);
+            });
+    }
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton></Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="postContent">
+                            <Form.Control
+                                placeholder="What is happening?!"
+                                as="textarea"
+                                rows={3}
+                                onChange={(e) => setPostContent(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="primary"
+                        className="rounded-pill"
+                        onClick={handleSave}
+                    >
+                        Tweet
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,7 +1,18 @@
 import { Button, Col, Image, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
-export default function ProfilePostCard({ content }) {
+export default function ProfilePostCard({ content, postId }) {
+    const [likes, setLikes] = useState(0);
     const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+
+    useEffect(() => {
+        fetch(
+            `https://e0dc5cb0-de85-4b0b-a831-1d38b0384bcf-00-1vz07zgn5c1sd.pike.replit.dev/likes/post/${postId}`
+        )
+            .then((response) => response.json())
+            .then((data) => setLikes(data.length))
+            .catch((error) => console.error("Error:", error));
+    }, [postId]);
 
     return (
         <Row
@@ -27,7 +38,7 @@ export default function ProfilePostCard({ content }) {
                         <i className="bi bi-repeat"></i>
                     </Button>
                     <Button variant="light">
-                        <i className="bi bi-heart"></i>
+                        <i className="bi bi-heart"> {likes}</i>
                     </Button>
                     <Button variant="light">
                         <i className="bi bi-graph-up"></i>
